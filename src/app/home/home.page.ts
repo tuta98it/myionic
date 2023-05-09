14453f6283d990f9e694a82bd1f2c2414ce5d37a
Trần Anh Tú <47886966+TranAnhTu1998@users.noreply.github.com>
2023-05-09 16:30:22 +0700

    Update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  passwordLogin = '';
+  showPassword = false;
   constructor() {}
 
+  togglePassword(){
+    this.showPassword = !this.showPassword;
+  }
+
+  forgotPassword() {
+    // Xử lý khi người dùng nhấn vào nút quên mật khẩu
+    // Chuyển hướng đến trang quên mật khẩu hoặc hiển thị form quên mật khẩu...
+  }
 }
