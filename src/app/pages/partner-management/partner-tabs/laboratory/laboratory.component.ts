1034f9a617a396f48b939e03cc382e8342dad991
trananhtu <tutran1998.tt@gmail.com>
2023-05-11 16:44:35 +0700

    Update UI Tab Labotory 051123

--- src/app/pages/partner-management/partner-tabs/laboratory/laboratory.component.ts
+++ src/app/pages/partner-management/partner-tabs/laboratory/laboratory.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-laboratory',
   templateUrl: './laboratory.component.html',
   styleUrls: ['./laboratory.component.scss'],
 })
 export class LaboratoryComponent  implements OnInit {
+
+  listPartnerLab = [
+    {
+      name: '1234-1643 Trần Văn A',
+      phone: '0981123574',
+      address: 'CT6 Khu đô thị Việt Hưng, Long Biên, Hà Nội',
+      conditon: 'Viêm loét dạ dày tá tràng',
+      status: 'Chưa nhận yêu cầu',
+    },
+
+    {
+      name: '1234 - 2341 Trần Thị Lý',
+      phone: '0234538592',
+      address: '257 Giải phóng, Hai Bà Trưng, Hà Nội',
+      conditon: 'Sốt xuất huyết',
+      status: 'Đã nhận yêu cầu và đang xử lý',
+    },
+
+    {
+      name: 'Lý Thuỳ Linh',
+      phone: '0903245394',
+      address: '40 Phường Liệt, Thanh Xuân, Hà Nội',
+      conditon: 'Viêm gan A',
+      status: 'Đã trả kết quả',
+    }
+
+  ]
 
   constructor() { }
 
--- src/app/pages/partner-management/partner-tabs/laboratory/laboratory.module.ts
+++ src/app/pages/partner-management/partner-tabs/laboratory/laboratory.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { LaboratoryComponent } from './laboratory.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [LaboratoryComponent],
-  imports: [IonicModule.forRoot(), RouterModule.forChild([{ path: '', component: LaboratoryComponent }])],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    RouterModule.forChild([{ path: '', component: LaboratoryComponent }]),
+  ],
   exports: [LaboratoryComponent]
 })
 export class LaboratoryModule { }
