4988368c368424ca0d6e0ee95bdd64330f9d4305
trananhtu <tutran1998.tt@gmail.com>
2023-05-16 11:20:53 +0700

    Update UI Page isShow Error Mess Input Pass and Us

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { IsEmptyPipe } from '../pipes/is-empty.pipe';
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
@@ -9,9 +10,59 @@ import { Router } from '@angular/router';
 
 
 export class LoginPage {
-  passwordLogin = '';
+  accountLogin = {
+    username: '',
+    password: '',
+    error: ''
+  }
+  isShowIsEmptyUsername = false;
+  isShowIsEmptyPassword = false;
   showPassword = false;
+
+  listAccounts = [
+    {
+      username: 'linhtl',
+      password: '9167',
+      name: '',
+      code: '',
+      phone: '',
+      address: '',
+      socialNetwork: {
+        facebook: '',
+        zalo: '',
+        telegram: '',
+        twitter: '',
+      }
+    },
+    {
+      username: 'tuta',
+      password: '9178',
+      name: '',
+      code: '',
+      phone: '',
+      address: '',
+      socialNetwork: {
+        facebook: '',
+        zalo: '',
+        telegram: '',
+        twitter: '',
+      }
+    },
+  ]
+
   constructor(private router: Router) {
+  }
+
+  isEmpty(value: any) {
+    return new IsEmptyPipe().transform(value);
+  }
+
+  isShowErrMessUsername(){
+    this.isShowIsEmptyUsername =  this.isEmpty(this.accountLogin.username);
+  }
+
+  isShowErrMessPassword(){
+    this.isShowIsEmptyPassword = this.isEmpty(this.accountLogin.password);
   }
 
   togglePassword() {
@@ -25,6 +76,31 @@ export class LoginPage {
 
 
   loginAccount() {
-    this.router.navigate(['/partner-management'])
+
+    let acc = this.accountLogin;
+    let listAcc = this.listAccounts;
+
+    let listAccIncludesUsername = listAcc.filter((objAcc) => {
+      return objAcc.username == acc.username;
+    });
+
+    if (this.isEmpty(listAccIncludesUsername)) {
+      this.accountLogin.error = 'Tên đăng nhập hoặc mật khẩu không hợp lệ'
+    } else {
+      let accIncludesPassword = listAccIncludesUsername.find((objAcc) =>
+        objAcc.password === acc.password
+      );
+
+      if (this.isEmpty(accIncludesPassword)) {
+        this.accountLogin.error = 'Tên đăng nhập hoặc mật khẩu không hợp lệ';
+      } else {
+        this.router.navigate(['/partner-management'])
+      }
+    }
+
   }
+
+
+
+
 }
