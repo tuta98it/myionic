a0c702a8609947e81a580e4bb75cf66ec4380e10
trananhtu <tutran1998.tt@gmail.com>
2023-05-16 09:47:14 +0700

    Update Partner

--- src/app/pages/partner-management/partner-management.page.spec.ts
+++ src/app/pages/partner-management/partner-management.page.spec.ts
@@ -15,3 +15,7 @@ describe('PartnerManagementPage', () => {
     expect(component).toBeTruthy();
   });
 });
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
+
