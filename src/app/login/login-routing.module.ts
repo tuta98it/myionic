7d685c6ed4e0cdcc71c84c7246a833f83ad1224e
trananhtu <tutran1998.tt@gmail.com>
2023-05-16 09:43:44 +0700

    Upadate Page Login

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPage } from './login';
+import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { LoginPage } from './login';
+import { LoginPage } from './login.page';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
